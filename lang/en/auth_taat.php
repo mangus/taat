aebd11fe8f9a846f613d063e9a6b967a6c6f136f
Mart Mangus <mart@mangus.ee>
2014-01-23 16:58:24 +0200

    Little cleanup

--- lang/en/auth_taat.php
+++ lang/en/auth_taat.php
@@ -1,6 +1,5 @@
 <?php
 
-//$string['auth_est_id_carddescription'] = 'Login with';
 $string['pluginname'] = 'Login with TAAT';
 $string['login_with'] = 'Login with TAAT';
 
--- lang/et/auth_taat.php
+++ lang/et/auth_taat.php
@@ -1,6 +1,5 @@
 <?php
 
-//$string['auth_est_id_carddescription'] = 'Login with';
 $string['pluginname'] = 'TAAT-i kaudu sisselogimine';
 $string['login_with'] = 'TAAT-i kaudu sisselogimine';
 
